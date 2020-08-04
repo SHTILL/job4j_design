9ef8658a959b22831d408d8df055a2f284cd5338
Semen Slepakov <educationkvv@mail.ru>
2020-08-04 15:36:37 +0300

    1. Что такое итератор.[4951#336049]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

---
+++ chapter_001/src/test/java/ru/job4j/it/BackwardArrayItTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.it;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class BackwardArrayItTest {
+    @Test
+    public void whenMultiCallhasNextThenTrue() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertTrue(it.hasNext());
+        assertTrue(it.hasNext());
+    }
+
+    @Test
+    public void whenReadSequence() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextFromEmpty() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {}
+        );
+        it.next();
+    }
+}
