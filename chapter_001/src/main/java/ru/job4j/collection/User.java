9bf6df013d0e0f8446979c04e3d72b16234a8b6f
Semen Slepakov <educationkvv@mail.ru>
2020-09-01 16:07:39 +0300

    1. Создать модель User[999#336068]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

---
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Calendar;
+
+public class User {
+    String name;
+    int children;
+    Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
