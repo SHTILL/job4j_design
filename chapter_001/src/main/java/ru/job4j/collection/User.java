8951c4b349fb7985ebfffebcefa6438cd9942c81
Semen Slepakov <educationkvv@mail.ru>
2020-09-02 12:13:07 +0300

    3. Переопределить только hashCode[1003#336070]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -11,5 +12,15 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
