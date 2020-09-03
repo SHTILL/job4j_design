71425c1cb300345b20786c7bf4454d0cb692fa16
Semen Slepakov <educationkvv@mail.ru>
2020-09-03 13:51:42 +0300

    4. Переопределить только equals[1004#336071]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -16,11 +16,15 @@ public class User {
 
     @Override
     public boolean equals(Object o) {
-        return super.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
     }
 }
