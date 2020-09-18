28fb7981d95ea34c50aa5d7f704c196efeb8dd2b
Semen Slepakov <educationkvv@mail.ru>
2020-09-18 17:54:20 +0300

    2. Статистику по коллекции.[45889#336034]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

--- chapter_001/src/main/java/ru/job4j/statistic/Analyze.java
+++ chapter_001/src/main/java/ru/job4j/statistic/Analyze.java
@@ -3,15 +3,14 @@ package ru.job4j.statistic;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Analyze {
     public static Info diff(List<User> previous, List<User> current) {
         Info info = new Info();
 
-        Map<Integer, String> prev = new HashMap<>();
-        for (User u: previous) {
-            prev.put(u.id, u.name);
-        }
+        Map<Integer, String> prev = previous.stream().collect(Collectors.toMap(u -> u.id, u -> u.name));
+
         for (User cur: current) {
             String prevName = prev.get(cur.id);
             if (prevName == null) {
