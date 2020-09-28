c9a58eb4555e69c857bb1948ad7ca2fd771b2cfd
Semen Slepakov <educationkvv@mail.ru>
2020-09-28 12:14:27 +0300

    0.1. FileOutputStream.[252488#336088]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

--- chapter_002/src/main/java/ru/job4j/io/Multiple.java
+++ chapter_002/src/main/java/ru/job4j/io/Multiple.java
@@ -7,23 +7,13 @@ import java.io.IOException;
 
 public class Multiple {
     public static void main(String[] args) {
-        FileWriter fos = null;
-        try {
-            fos = new FileWriter("multiply_table.txt");
+        try (FileWriter fos = new FileWriter("multiply_table.txt")) {
             fos.write("1*2=2" + System.lineSeparator());
             fos.write("1*3=3" + System.lineSeparator());
             fos.write("-----------" + System.lineSeparator());
             fos.write("1*9=9" + System.lineSeparator());
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
