bd8edcabafdfe459bc98a604738d3438b4498f79
Semen Slepakov <educationkvv@mail.ru>
2020-09-30 15:49:18 +0300

    1. Читаем файл конфигурации[858#336094]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

--- chapter_002/src/main/java/ru/job4j/io/Config.java
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -20,7 +20,7 @@ public class Config {
 
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
             String s = null;
-            Pattern p = Pattern.compile("^[\\w&&[^#]][\\w&&[^=]]+=[\\S&&[^=]]+$");
+            Pattern p = Pattern.compile("^\\w+=[\\S&&[^=]]+$");
             do {
                 s = read.readLine();
                 if (s != null) {
@@ -53,7 +53,7 @@ public class Config {
 
     public static void main(String[] args) {
         System.out.println(new Config("./chapter_002/data/pair_without_comment.properties"));
-        Config c = new Config("./chapter_002/data/pair_without_comment.properties");
+        Config c = new Config("./chapter_002/data/pairs_multiple.properties");
         c.load();
     }
 }
