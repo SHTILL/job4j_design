923a9c8c9e758c53452a38cebd80e01a475824d8
Semen Slepakov <educationkvv@mail.ru>
2020-10-01 13:49:03 +0300

    2. Анализ доступности сервера.[859#336095]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

--- chapter_002/src/main/java/ru/job4j/io/Analyze.java
+++ chapter_002/src/main/java/ru/job4j/io/Analyze.java
@@ -1,40 +1,53 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analyze {
     public static void unavailable(String source, String target) {
         String s;
+        List<String> unavailableTime = new ArrayList<>();
+        String tmp = null;
+
         boolean isDown = false;
         boolean isDownPrev = false;
-        try (
-                BufferedReader r = new BufferedReader(new FileReader(source));
-                PrintWriter w = new PrintWriter(new FileOutputStream(target))
-        ) {
+        try (BufferedReader r = new BufferedReader(new FileReader(source))) {
             while (true) {
-               s = r.readLine();
-               if (s == null) {
+                s = r.readLine();
+                if (s == null) {
                     break;
-               }
+                }
 
-               String[] tokens = s.split(" ", 2);
-               String error = tokens[0];
-               String time = tokens[1];
+                String[] tokens = s.split(" ", 2);
+                String error = tokens[0];
+                String time = tokens[1];
 
-               isDown = !error.equals("200") && !error.equals("300");
+                isDown = !error.equals("200") && !error.equals("300");
 
-               if (isDown != isDownPrev) {
-                   if (isDown) {
-                       w.write(time + ";");
-                   } else {
-                       w.write(time + ";" + System.lineSeparator());
-                   }
-                   isDownPrev = isDown;
-               }
+                if (isDown != isDownPrev) {
+                    if (isDown) {
+                        tmp = time + ";";
+                    } else {
+                        unavailableTime.add(tmp + time + ";" + System.lineSeparator());
+                    }
+                    isDownPrev = isDown;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        if (isDown) {
+            unavailableTime.add(tmp + System.lineSeparator());
+        }
+
+        try (PrintWriter w = new PrintWriter(new FileOutputStream(target))) {
+            unavailableTime.forEach(w::write);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
