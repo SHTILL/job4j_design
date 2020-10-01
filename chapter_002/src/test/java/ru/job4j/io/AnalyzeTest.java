cd3833b2804190814ac0ae6a195ba5e8e26dc986
Semen Slepakov <educationkvv@mail.ru>
2020-10-01 17:59:02 +0300

    2. Анализ доступности сервера.[859#336095]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

--- chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,61 +1,90 @@
 package ru.job4j.io;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalyzeTest {
+    @Rule
+    public TemporaryFolder f = new TemporaryFolder();
+
     @Test
-    public void whenExample() {
-        String source = "./data/server.log";
-        String target = "./data/unavailable.csv";
-        Analyze.unavailable(source, target);
-        try (BufferedReader r = new BufferedReader(new FileReader(target))) {
-            assertThat(r.readLine(), is("10:57:01;10:59:01;"));
-            assertThat(r.readLine(), is("11:01:02;11:02:02;"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void whenExample() throws IOException {
+        String sourceFileName = "server.log";
+        String targetFileName = "unavailable.csv";
+        File source = f.newFile(sourceFileName);
+        Writer w = new BufferedWriter(new FileWriter(source));
+        w.write("200 10:56:01" + System.lineSeparator());
+        w.write("500 10:57:01" + System.lineSeparator());
+        w.write("400 10:58:01" + System.lineSeparator());
+        w.write("200 10:59:01" + System.lineSeparator());
+        w.write("500 11:01:02" + System.lineSeparator());
+        w.write("200 11:02:02" + System.lineSeparator());
+        w.close();
+
+        File target = f.newFile(targetFileName);
+        Analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        BufferedReader r = new BufferedReader(new FileReader(target));
+        assertThat(r.readLine(), is("10:57:01;10:59:01;"));
+        assertThat(r.readLine(), is("11:01:02;11:02:02;"));
+        r.close();
     }
 
     @Test
-    public void whenStartDown() {
-        String source = "./data/server_start_down.log";
-        String target = "./data/unavailable.csv";
-        Analyze.unavailable(source, target);
-        try (BufferedReader r = new BufferedReader(new FileReader(target))) {
-            assertThat(r.readLine(), is("10:57:01;10:58:01;"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void whenStartDown() throws IOException {
+        String sourceFileName = "server.log";
+        String targetFileName = "unavailable.csv";
+        File source = f.newFile(sourceFileName);
+        Writer w = new BufferedWriter(new FileWriter(source));
+        w.write("500 10:57:01" + System.lineSeparator());
+        w.write("200 10:58:01" + System.lineSeparator());
+        w.close();
+
+        File target = f.newFile(targetFileName);
+        Analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        BufferedReader r = new BufferedReader(new FileReader(target));
+        assertThat(r.readLine(), is("10:57:01;10:58:01;"));
+        r.close();
     }
 
     @Test
-    public void whenStartUp() {
-        String source = "./data/server_start_up.log";
-        String target = "./data/unavailable.csv";
-        Analyze.unavailable(source, target);
-        try (BufferedReader r = new BufferedReader(new FileReader(target))) {
-            assertThat(r.readLine(), is("10:57:01;10:58:01;"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void whenStartUp() throws IOException {
+        String sourceFileName = "server.log";
+        String targetFileName = "unavailable.csv";
+        File source = f.newFile(sourceFileName);
+        Writer w = new BufferedWriter(new FileWriter(source));
+        w.write("300 10:20:01" + System.lineSeparator());
+        w.write("500 10:57:01" + System.lineSeparator());
+        w.write("200 10:58:01" + System.lineSeparator());
+        w.close();
+
+        File target = f.newFile(targetFileName);
+        Analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        BufferedReader r = new BufferedReader(new FileReader(target));
+        assertThat(r.readLine(), is("10:57:01;10:58:01;"));
+        r.close();
     }
 
     @Test
-    public void whenStillDown() {
-        String source = "./data/server_still_down.log";
-        String target = "./data/unavailable.csv";
-        Analyze.unavailable(source, target);
-        try (BufferedReader r = new BufferedReader(new FileReader(target))) {
-            assertThat(r.readLine(), is("10:57:01;"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void whenStillDown() throws IOException {
+        String sourceFileName = "server.log";
+        String targetFileName = "unavailable.csv";
+        File source = f.newFile(sourceFileName);
+        Writer w = new BufferedWriter(new FileWriter(source));
+        w.write("300 10:20:01" + System.lineSeparator());
+        w.write("500 10:57:01" + System.lineSeparator());
+        w.close();
+
+        File target = f.newFile(targetFileName);
+        Analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        BufferedReader r = new BufferedReader(new FileReader(target));
+        assertThat(r.readLine(), is("10:57:01;"));
+        r.close();
     }
 }
