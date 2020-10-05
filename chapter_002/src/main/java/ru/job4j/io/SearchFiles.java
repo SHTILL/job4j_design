1c1261a6578dc8b165524841b83d798742d3f564
Semen Slepakov <educationkvv@mail.ru>
2020-10-05 13:59:12 +0300

    4.1. Сканирование файловой системы.[106929#336085]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

---
+++ chapter_002/src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,47 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles implements FileVisitor<Path> {
+    Predicate<Path> p;
+    List<Path> files = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> p) {
+        this.p = p;
+    }
+
+    List<Path> getPaths() {
+        return files;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (p.test(file)) {
+            files.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+}
