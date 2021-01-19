08a201a3698fb1f9be6a0ff086ccc0d7f8e56050
Semen Slepakov <educationkvv@mail.ru>
2021-01-19 11:44:15 +0300

    0. Видео[6863#336000]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

---
+++ chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,20 @@
+package ru.job4j.jdbc;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
