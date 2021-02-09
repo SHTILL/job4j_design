ff785fa804ea9b5626e2960bbff6f338ac65b172
Semen Slepakov <educationkvv@mail.ru>
2021-02-09 13:39:23 +0300

    0. Видео[6854#336151]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

--- chapter_004/src/main/java/ru/job4j/gc/ref/PhantomDemo.java
+++ chapter_004/src/main/java/ru/job4j/gc/ref/PhantomDemo.java
@@ -1,6 +1,7 @@
 package ru.job4j.gc.ref;
 
 import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,9 +9,7 @@ import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;
 
 public class PhantomDemo {
-
     private static class MyPhantom extends PhantomReference<String> {
-
         private String name;
 
         public MyPhantom(String referent, ReferenceQueue<? super String> q, String name) {
@@ -39,15 +38,24 @@ public class PhantomDemo {
             for (ListIterator<MyPhantom> i = phantoms.listIterator(); i.hasNext();) {
                 MyPhantom current = i.next();
                 if (current != null && current.isEnqueued()) {
-                    System.out.println("Utilized " + current.get());
+                    //System.out.println("Utilized " + current.get());
                     current.clear();
                     i.remove();
                 }
             }
         }
+
+        public void utilizeResourceCorrect() {
+            Reference ref = null;
+            while ((ref = queue.poll()) != null) {
+                //System.out.println("Utilized " + ref.get());
+                ref.clear();
+                phantoms.remove(0);
+            }
+        }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    private static void example1() throws InterruptedException {
         PhantomStorage storage = new PhantomStorage();
         String data = "123".repeat(1000);
         storage.add(data);
@@ -55,6 +63,40 @@ public class PhantomDemo {
         System.gc();
         TimeUnit.SECONDS.sleep(3);
         storage.utilizeResource();
+    }
+
+    private static void example2() throws InterruptedException {
+        PhantomStorage storage = new PhantomStorage();
+        for (int i = 0; i < 100_000; i++) {
+            String data = Integer.toString(i);
+            storage.add(data);
+            storage.utilizeResource();
+            if (i % 1000 == 0) {
+                System.out.println("*");
+                System.gc();
+            }
+        }
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    private static void example3() throws InterruptedException {
+        PhantomStorage storage = new PhantomStorage();
+        for (int i = 0; i < 100_000; i++) {
+            String data = Integer.toString(i);
+            storage.add(data);
+            storage.utilizeResourceCorrect();
+            if (i % 1000 == 0) {
+                System.out.println("*");
+                System.gc();
+            }
+        }
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        //example1();
+        example2();
+        //example3();
     }
 
 }
