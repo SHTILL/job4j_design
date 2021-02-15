3ceb2dffb3aeff4fbf6dbd807ebc0a2322bdf807
Semen Slepakov <educationkvv@mail.ru>
2021-02-15 15:47:06 +0300

    3. Шаблонизатор.[855#336115]
    Signed-off-by: Semen Slepakov <educationkvv@mail.ru>

---
+++ chapter_005/src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest {
+    @Test
+    public void whenGenerated() {
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        Generator g = new SimpleGenerator();
+        assertThat(g.produce("\"I am a ${name}, Who are ${subject}? \"", args), is("I am a Petr Arsentev, Who are you? "));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongArgs() {
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        Generator g = new SimpleGenerator();
+        g.produce("\"I am a ${name}, Who are ${subject}? \"", args);
+    }
+}
